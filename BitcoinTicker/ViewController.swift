038d30ac4cbfd934d7b373ac546a6a3016d7f7d9
psx95 <pranav.ps95@hotmail.com>
2019-02-04 01:42:06 +0530

    feat(App): Implements API calls and updates UI with response

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
@@ -17,69 +19,89 @@ class ViewController: UIViewController {
     //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var loadingAnimator: UIActivityIndicatorView!
+    @IBOutlet weak var loadingView: UIView!
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        hideLoadingViews()
+        //loadingAnimator.stopAnimating()
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    
-    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
     
-    
-    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print (currencyArray[row])
+        
+        finalURL = baseURL + currencyArray[row]
+        getBitcoinData(url: finalURL)
+    }
 //    
 //    //MARK: - Networking
 //    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
-//    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    
+    func getBitcoinData(url: String) {
+        showLoadingViews()
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    self.hideLoadingViews()
+                   // self.loadingAnimator.stopAnimating()
+                    let bitcoinJSON : JSON = JSON(response.result.value!)
+                    self.updateBitcoinJSON(json: bitcoinJSON)
 
+                } else {
+                    self.hideLoadingViews()
+            //        self.loadingAnimator.stopAnimating()
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "⚠️"
+                }
+            }
 
+    }
 
-
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    func updateBitcoinJSON(json : JSON) {
+        
+        if let priceResult = json["ask"].double {
+            bitcoinPriceLabel.text = String(priceResult)
+        } else {
+            bitcoinPriceLabel.text = "N/A"
+        }
+    }
+    
+    func hideLoadingViews() {
+        self.loadingLabel.isHidden = true
+        self.loadingAnimator.stopAnimating()
+    }
+    
+    func showLoadingViews() {
+        self.bitcoinPriceLabel.text = "Price"
+        self.loadingLabel.isHidden = false
+        self.loadingAnimator.startAnimating()
+    }
 }
 
